45b0c816f9f276b10ee42563fbe25b510b319f0e
wade-lab <72329748+wade-lab@users.noreply.github.com>
2021-09-13 23:41:23 -0400

    Add files via upload

---
+++ fastq file trimming for polyadenylated samples 9_13_21.py
@@ -0,0 +1,26 @@
+
+
+input_fastq_file = input("Enter the path and filename for the .fastq file to be trimmed\n")
+output_fastq_file = input("Enter the path and filename for the output .fastq file\n")
+
+f = open(input_fastq_file,'r')
+trimmed_fastq = []
+while True:
+	a=f.readline()
+	if a=='':
+		break
+	b=f.readline()
+	c=f.readline()
+	d=f.readline()
+	if 45 > b.find('AAA') > 20:
+		trimmed_fastq.append(a)
+		trimmed_fastq.append(b[0:b.find('AAA')]+'\n')
+		trimmed_fastq.append(c)
+		trimmed_fastq.append(d[0:b.find('AAA')]+'\n')
+
+f.close()
+f=open(output_fastq_file,'w')
+f.writelines(trimmed_fastq)
+f.close()
+
+ 
